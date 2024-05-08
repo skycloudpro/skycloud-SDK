38d3338103b36d50f96f08bf3d6121afd071b302
skycloudpro <frankie@scloud.live>
2024-05-08 02:22:55 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,15 +1,17 @@
 include'sCloud_SDK.php';
-
- $sCloud = new sCloud( $dummy, 15,2,0 );
+$sCloud = new sCloud( $dummy, 15,2,0 );
 
 $sCloud->generateAuthUrl(1);
- $token=$sCloud->API('token');//json format
+$token=$sCloud->API('token');//json format
  
- //Example API calls...
+//Example API calls... (refer to the full API call list on sclouds developer website)
+// https://developers.scloud.live/Developer/Documentation/#APIs
+
 $userInfo=   $sCloud->API('account_info');//json format
 $userEmail=  $sCloud->API('account_info')['registered_email'];//json format
 $storageUsed=$sCloud->API('account_info')['used_storage'];//json format
 $randImage=$sCloud->API('images')['22'];//json format
 $sCloud->API('Videos')['count'];
-  
+
+//helper methods.
 $sCloud->AuthToken();//get authentication token info, expiry, creation date
